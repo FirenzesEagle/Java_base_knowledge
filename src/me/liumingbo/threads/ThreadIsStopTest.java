86903622ea07fbf6d12da4f559c2a1f95bf7f460
FirenzesEagle <liumingbo2008@gmail.com>
2016-12-09 21:43:00 +0800

    fulfill thread usage & readme

---
+++ src/me/liumingbo/threads/ThreadIsStopTest.java
@@ -0,0 +1,36 @@
+package me.liumingbo.threads;
+
+/**
+ * Created by FirenzesEagle on 2016/12/9.
+ * Email:liumingbo2008@gmail.com
+ */
+public class ThreadIsStopTest {
+    public static void main(String[] args) {
+        ThreadIsStopTest threadIsStopTest = new ThreadIsStopTest();
+        MyThread thread = threadIsStopTest.new MyThread();
+        thread.start();
+        try {
+            System.out.println("当前线程为：" + Thread.currentThread().getName());
+            Thread.currentThread().sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.setStop(true);
+    }
+
+    class MyThread extends Thread{
+        private volatile boolean isStop = false;
+        @Override
+        public void run() {
+            int i = 0;
+            while(!isStop){
+                System.out.println(i + " while循环");
+                i++;
+            }
+        }
+
+        public void setStop(boolean stop){
+            this.isStop = stop;
+        }
+    }
+}
